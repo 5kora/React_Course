ba90cab5b9e6cccf810845df962aabb3ba0d96b8
Marcos Araujo <araujo678@gmail.com>
2023-03-04 20:42:27 -0300

    Aula 55

--- 5_FORMS_REACT/forms/src/components/MyForm.js
+++ 5_FORMS_REACT/forms/src/components/MyForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './MyForm.css'
 
 const MyForm = () => {
   return (
@@ -6,7 +7,7 @@ const MyForm = () => {
         {/* Criação de form */}
         <form>
             <div>
-                <label htmlFor="name">Nome:</label>
+                <label htmlFor="name">Nome: </label>
                 <input type="text" name='name' placeholder='Digite o seu nome' />
             </div>
             <input type="submit" value="Enviar"/>
