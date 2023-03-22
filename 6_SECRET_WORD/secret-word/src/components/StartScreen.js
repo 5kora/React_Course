8e02a7e332901ba2f3f65ee6ec54a3358baedbed
Marcos Araujo <araujo678@gmail.com>
2023-03-22 20:27:03 -0300

    Pagina inicial DONE

--- 6_SECRET_WORD/secret-word/src/components/StartScreen.js
+++ 6_SECRET_WORD/secret-word/src/components/StartScreen.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const StartScreen = () => {
   return (
-    <div>
+    <div className='start'>
         <h1>Secret Word</h1>
+        <p>Clique no botão para começar a jogar!</p>
+        <button>COMEÇAR O JOGO</button>
     </div>
   )
 }
-
+ 
 export default StartScreen
