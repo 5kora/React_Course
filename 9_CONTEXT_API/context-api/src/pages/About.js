2297837117991d15a423f537d6128d16f55a783b
Marcos <araujo678@gmail.com>
2023-07-19 09:26:58 -0300

    Inicio projeto MiniBlog

--- 9_CONTEXT_API/context-api/src/pages/About.js
+++ 9_CONTEXT_API/context-api/src/pages/About.js
@@ -1,11 +1,15 @@
 import { useCounterContext } from "../hooks/useCounterContext";
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 const About = () => {
   const {counter} = useCounterContext();
+
+   // // 5 - Context complexo
+   const { color } = useTitleColorContext();
   
   return (
     <div>
-      <h1>About</h1>
+      <h1 style={{color:color}}>About</h1>
       <p>Valor do contador: {counter}</p>
     </div>
   )
--- 9_CONTEXT_API/context-api/src/pages/Home.js
+++ 9_CONTEXT_API/context-api/src/pages/Home.js
@@ -10,17 +10,27 @@ import { useTitleColorContext } from '../hooks/useTitleColorContext';
 
 const Home = () => {
   // const {counter} = useContext(CounterContext)
-  const {counter} = useCounterContext();
+  const { counter } = useCounterContext();
 
-  // 5 - Context complexo
-  const {colorContext} = useTitleColorContext();
-  
+  // // 5 - Context complexo
+  const { color, dispatch } = useTitleColorContext();
+
+  // 6 - Alterando state complexo
+  const setTitleColor = (color) => {
+    dispatch({ type: color });
+  }
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1 style={{ color: color }}> Home</h1>
       <p>Valor do contador: {counter}</p>
       {/* 3 - Alterando context */}
-      <ChangeCounter/>
+      <ChangeCounter />
+      {/* 6 - Alterando contexto complexo */}
+      <div>
+        <button onClick={() => {setTitleColor("RED")}}>Vermelho</button>
+        <button onClick={() => {setTitleColor("BLUE")}}>Azul</button>
+      </div>
     </div>
   )
 }
