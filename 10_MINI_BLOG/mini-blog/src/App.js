99031021e1c0719ef2dc46880263c66d99bf1046
Marcos <araujo678@gmail.com>
2023-12-01 18:06:52 -0300

    miniblog nao lembro mais nada

--- 10_MINI_BLOG/mini-blog/src/App.js
+++ 10_MINI_BLOG/mini-blog/src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import { db } from "./firebase/config";
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
+//Components
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+//Pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 
@@ -8,12 +14,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
       <div className="container">
         <Routes>
           <Route path = '/' element={<Home/>}/>
           <Route path = '/about' element={<About/>}/>
         </Routes>
       </div>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
