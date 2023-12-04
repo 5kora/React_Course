f294d3c0f1d708d8125d8d55a2b816d8a0627b4a
Marcos <araujo678@gmail.com>
2023-12-04 12:04:55 -0300

    Inicio pagina de registro

--- 10_MINI_BLOG/mini-blog/src/App.js
+++ 10_MINI_BLOG/mini-blog/src/App.js
@@ -9,6 +9,8 @@ import Navbar from "./components/Navbar";
 //Pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
@@ -18,7 +20,9 @@ function App() {
       <div className="container">
         <Routes>
           <Route path = '/' element={<Home/>}/>
-          <Route path = '/about' element={<About/>}/>
+          <Route path = '/about' element={<About/>}/>.
+          <Route path = '/login' element={<Login/>}/>
+          <Route path = '/register' element={<Register/>}/>
         </Routes>
       </div>
       <Footer/>
--- 10_MINI_BLOG/mini-blog/src/pages/About/About.js
+++ 10_MINI_BLOG/mini-blog/src/pages/About/About.js
@@ -5,7 +5,9 @@ import styles from "./About.module.css"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h2>About</h2>
+    </div>
   )
 }
 
