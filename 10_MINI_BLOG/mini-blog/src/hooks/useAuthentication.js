81b026d7f208fe4dfd3857a5d673c4198c6b838d
Marcos <araujo678@gmail.com>
2023-12-26 18:11:51 -0300

    estrutura pagina de novo post

--- 10_MINI_BLOG/mini-blog/src/hooks/useAuthentication.js
+++ 10_MINI_BLOG/mini-blog/src/hooks/useAuthentication.js
@@ -64,10 +64,34 @@ export const useAuthentication = () => {
   };
 
   //logout - sign out
-
   const logout = () => {
     checkIfIsCancelled();
     signOut(auth);
+  };
+
+  //login - sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (e) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -79,6 +103,7 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
-    logout
+    logout,
+    login
   };
 };
